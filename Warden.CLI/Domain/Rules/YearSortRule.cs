68c4d218a3638799356353c0810d02031d55b574
Saul Enriquez <saul.enriquez2005@gmail.com>
2026-04-01 15:32:52 -0500

    fix(rules): fallback to _Unknown for invalid dates

--- Warden.CLI/Domain/Rules/YearSortRule.cs
+++ Warden.CLI/Domain/Rules/YearSortRule.cs
@@ -6,14 +6,9 @@ namespace Warden.CLI.Domain.Rules
     {
         public string GetSubFolderName(FileInfo file)
         {
-            if (file.LastWriteTime > DateTime.Now)
+            if (file.LastWriteTime.Year < 1700 || file.LastWriteTime > DateTime.Now)
             {
-                throw new InvalidOperationException("File is from the future!");
-            }
-
-            if (file.LastWriteTime.Year < 1700)
-            {
-                throw new ArgumentOutOfRangeException("File is from the past!");
+                return "_Unknown";
             }
 
             return file.LastWriteTime.Year.ToString();
