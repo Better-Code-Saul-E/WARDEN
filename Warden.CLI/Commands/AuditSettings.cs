1d6fd596d86e704a436d388439061bb5b45ae58a
Saul Enriquez <saul.enriquez2005@gmail.com>
2026-04-07 15:31:14 -0500

    fix(cli): correct misleading description for audit --limit flag

--- Warden.CLI/Commands/AuditSettings.cs
+++ Warden.CLI/Commands/AuditSettings.cs
@@ -7,7 +7,7 @@ namespace Warden.CLI.Commands
     {
         [CommandOption("--limit|-l")]
         [DefaultValue(10)]
-        [Description("View and audit of your past actions. (e.g., -limit 25)")]
+        [Description("View and audit of your past actions. (e.g., -l 25)")]
         public int Limit { get; set; }
 
     }
